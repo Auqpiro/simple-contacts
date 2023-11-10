ba5690062032ba285d3294053034b3440e952efc
Ivan <zzoomzoone@gmail.com>
2023-11-11 02:42:36 +0300

    add "set deploy on heroku"
    set scripts
    refactor api instance

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,3 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "https://demo-json-api.vercel.app",
-    // baseURL: "http://localhost:5001",
-})
+export default axios.create();
