c78f6686f0e34de72f55fa81d8bf4f230adc5287
Ivan Konovalov <zzoomzoone@gmail.com>
2023-11-18 17:13:36 -0600

    add "work with styles"
    make work with styles of adding new contact page

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,7 +1,12 @@
 import {IContact} from "../../type";
 import {useForm} from "react-hook-form";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import Button from "../Button";
+import styles from "./form.module.scss";
+import {FaUserAlt} from "react-icons/fa";
+import { RiContactsLine } from "react-icons/ri";
+import { LuPhone } from "react-icons/lu";
+import { MdOutlineAlternateEmail } from "react-icons/md";
 
 type FormValues = Omit<IContact, "id">;
 
@@ -39,74 +44,87 @@ function ContactForm(props: FormProps) {
     const canSubmit: boolean = ("selected" in props) ? !(isValid && isDirty) : !isValid;
 
     return (
-        <form onSubmit={handleSubmit(props.onSubmit)}>
-            <div>
-                <label>
-                    Name
-                    <input
-                        defaultValue={"selected" in props ? props.selected.name : ""}
-                        {...register("name", {
-                            required: {
-                                value: true,
-                                message: "required"
-                            },
-                            minLength: {
-                                value: 2,
-                                message: "min length 2 symbols"
-                            },
-                        })}
-                    />
-                    {errors?.name && <p>{errors.name.message ?? "error"}</p>}
-                </label>
-            </div>
-            <div>
-                <label>
-                    Phone
-                    <input
-                        type="tel"
-                        defaultValue={"selected" in props ? props.selected.phone : ""}
-                        {...register("phone", {
-                            required: {
-                                value: true,
-                                message: "required"
-                            },
-                            pattern: {
-                                value: /^\+?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/,
-                                message: "not match phone number pattern"
-                            },
-                        })}
-                    />
-                    {errors?.phone && <p>{errors.phone.message ?? "error"}</p>}
-                </label>
-            </div>
-            <div>
-                <label>
-                    Email
-                    <input
-                        type="email"
-                        defaultValue={"selected" in props ? props.selected.email : ""}
-                        {...register("email", {
-                            required: {
-                                value: true,
-                                message: "required"
-                            },
-                            pattern: {
-                                value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-                                message: "not match email pattern"
-                            },
-                        })}
-                    />
-                    {errors?.email && <p>{errors.email.message ?? "error"}</p>}
-                </label>
-            </div>
-            <Button
-                variant={!canSubmit ? "primary" : "secondary"}
-                type="submit"
-                disabled={canSubmit}
-            >
-                SUBMIT
-            </Button>
-        </form>
+        <div className={styles.container}>
+            <form className={styles.content} onSubmit={handleSubmit(props.onSubmit)}>
+                <fieldset>
+                    <legend>
+                        <FaUserAlt/>
+                    </legend>
+                    <div className={styles.form_item}>
+                        <RiContactsLine/>
+                        <label>
+                            Name
+                        </label>
+                        <input
+                            placeholder="Your name"
+                            defaultValue={"selected" in props ? props.selected.name : ""}
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: "required"
+                                },
+                                minLength: {
+                                    value: 2,
+                                    message: "min length 2 symbols"
+                                },
+                            })}
+                        />
+                    </div>
+                    <p className={styles.error_message}>{errors?.name && (errors.name.message ?? "error")}</p>
+                    <div className={styles.form_item}>
+                        <LuPhone/>
+                        <label>
+                            Phone
+                        </label>
+                        <input
+                            type="tel"
+                            placeholder="Your phone"
+                            defaultValue={"selected" in props ? props.selected.phone : ""}
+                            {...register("phone", {
+                                // required: {
+                                //     value: true,
+                                //     message: "required"
+                                // },
+                                pattern: {
+                                    value: /^\+?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/,
+                                    message: "not match phone number pattern"
+                                },
+                            })}
+                        />
+                    </div>
+                    <p className={styles.error_message}>{errors?.phone && (errors.phone.message ?? "error")}</p>
+                    <div className={styles.form_item}>
+                        <MdOutlineAlternateEmail/>
+                        <label>
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Your email"
+                            defaultValue={"selected" in props ? props.selected.email : ""}
+                            {...register("email", {
+                                // required: {
+                                //     value: true,
+                                //     message: "required"
+                                // },
+                                pattern: {
+                                    value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+                                    message: "not match email pattern"
+                                },
+                            })}
+                        />
+                    </div>
+                    <p className={styles.error_message}>{errors?.email && (errors.email.message ?? "error")}</p>
+                </fieldset>
+                <Button
+                    variant={!canSubmit ? "primary" : "secondary"}
+                    type="submit"
+                    disabled={canSubmit}
+                >
+                    SUBMIT
+                </Button>
+            </form>
+        </div>
     )
 }
 
